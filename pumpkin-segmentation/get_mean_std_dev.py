f0d62ab0e7681fc1c67657c5556bf9e14614c705
Per Breum <lillep3r@hotmail.com>
2018-02-19 15:09:03 +0100

    corrected paths

--- pumpkin-segmentation/get_mean_std_dev.py
+++ pumpkin-segmentation/get_mean_std_dev.py
@@ -14,9 +14,9 @@ def load_pictures_rgb(path):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     return img
 
-def get_mean_st_dev(image_drawed, image_original):
-    image_drawed = load_pictures_rgb("DJI_0237_copy.JPG")
-    image_original = load_pictures_rgb("DJI_0237.JPG")
+def get_mean_st_dev(image_training, image_orig):
+    image_drawed = load_pictures_rgb(image_training)
+    image_original = load_pictures_rgb(image_orig)
     # red color is used to draw on pumpkins in test image
     # thresholding in red channel
     min_rgb_thres = np.array([240, 0, 0])
@@ -26,18 +26,20 @@ def get_mean_st_dev(image_drawed, image_original):
                                   image_drawed, image_original, color_chan)
 
     mean, std_dev = cv2.meanStdDev(image_original, mask=mask_image)
-    # print mean
-    # print std_dev
 
-    # used to show workign method by removing all but the pixels
-    # with valid threshold values
-    min_rgb = np.array(mean - std_dev)
-    max_rgb = np.array(mean + std_dev)
-    mask_rgb = cv2.inRange(image_original,min_rgb,max_rgb)
-    result_rgb = cv2.bitwise_and(image_original, image_original, mask=mask_rgb)
-    print_image(image_original, "Original image")
-    print_image(result_rgb, "Result for part 1")
-    plt.show()
+    ################################################################
+    ## used to show working method by removing all but the pixels
+    ## with valid threshold values
+
+    # min_rgb = np.array(mean - std_dev)
+    # max_rgb = np.array(mean + std_dev)
+    # mask_rgb = cv2.inRange(image_original,min_rgb,max_rgb)
+    # result_rgb = cv2.bitwise_and(image_original, image_original, mask=mask_rgb)
+    # print_image(image_original, "Original image")
+    # print_image(result_rgb, "Result for part 1")
+    # plt.show()
+
+    ################################################################
     return mean, std_dev
 
 # threshold a specified color and return the original image
@@ -50,7 +52,9 @@ def get_masked_image(min_thres, max_thres, image_drawed,
     return mask_rgb
 
 if __name__ == "__main__":
-    path_to_training_image = "DJI_0237_copy.JPG"
-    path_to_original_image = "DJI_0237.JPG"
+    path_to_training_image = "photos/DJI_0237_copy.JPG"
+    path_to_original_image = "photos/DJI_0237.JPG"
     mean, std_dev = get_mean_st_dev(path_to_training_image,
                                     path_to_original_image)
+    print mean
+    print std_dev
