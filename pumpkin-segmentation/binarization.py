7788f1a3f2130ac912018f1700c892367fb69e46
Javier <jlrandulfe@gmail.com>
2018-02-19 10:55:15 +0100

    Filter the saturation and brightness fields when binarizing

--- pumpkin-segmentation/binarization.py
+++ pumpkin-segmentation/binarization.py
@@ -2,11 +2,13 @@
 import cv2
 import numpy as np
 
-def hsv_binarization(image, h_max=20):
+def hsv_binarization(image, h_max=20, s_min=120, v_min=110):
     """Convert image to HSV and binarize it."""
     # Convert from RGB to HSV format.
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     # Filter the pixels according to the HSV values.
     h_bin = (hsv[:, :, 0] < h_max).astype(np.uint8)
-    binarized = h_bin
+    s_bin = (hsv[:, :, 1] > s_min).astype(np.uint8)
+    v_bin = (hsv[:, :, 2] > v_min).astype(np.uint8)
+    binarized = h_bin * s_bin * v_bin
     return binarized
